c4bbb773062d3c20e62b56d51abc75fd237f32ad
_DevNote <rommelagasa102@gmail.com>
2026-02-28 18:58:29 +0800

    - immediate and deep watchers

--- src/components/LearnWatchers.vue
+++ src/components/LearnWatchers.vue
@@ -7,6 +7,14 @@
         <button @click="volume -= 2">Decrease</button>
     </div>
 
+    <input type="text" v-model="movie">
+    <input type="text" v-model="movieDetails.genre">
+    <input type="text" v-model="movieDetails.director">
+
+    <div>
+        <button @click="movieList.push({name: 'Wonder Woman', genre: 'Action', director: 'Patty Jenkins'})">Add Movie</button>
+    </div>
+
 </template>
 
 
@@ -16,7 +24,17 @@ export default {
     name: 'LearnWatchers',
     data() {
         return {
-            volume: 0
+            volume: 0,
+            movie: 'Batman',
+            movieDetails: {
+                genre: '',
+                director: ''
+            },
+            movieList: [
+                { name: 'Batman', genre: 'Action', director: 'Christopher Nolan' },
+                { name: 'Inception', genre: 'Sci-Fi', director: 'Christopher Nolan' },
+                { name: 'The Dark Knight', genre: 'Action', director: 'Christopher Nolan' }
+            ]
         }
     },
     methods: {
@@ -30,7 +48,29 @@ export default {
             if(newValue > oldValue && newValue === 16){
                 alert('Listening to a high volume for a long time my damage your hearing.');
             }
+        },
+        movie: {
+            handler(newValue){
+                console.log('Calling API with movie name: ' + newValue)
+            },
+            immediate: true, // this will call the handler immediately when the component is created
+        },
+        movieDetails: {
+            handler(newValue){
+                console.log('Movie genre: ' + newValue.genre);
+                console.log('Movie director: ' + newValue.director);
+            },
+            deep: true // this will watch for changes in nested properties of movieDetails
+        },
+        movieList: {
+            handler(newValue){
+                console.log('Movie list updated: ' + JSON.stringify(newValue));
+            },
+            deep: true
         }
+        // movie(newValue){
+        //     console.log('Calling API with movie name: ' + newValue)
+        // }
     }
 }
 
